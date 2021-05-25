06711d02c3b02eab156810aa7421f489689326ac
Ki Yung Ahn <kyagrd@gmail.com>
2021-05-26 01:02:45 +0900

    add

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for par-uniq-id
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Lib
+
+import Control.Parallel.Strategies
+import Data.List
+
+qsort [] = []
+qsort [y] = [y]
+qsort (y:ys) = xs ++ y : zs
+    where (xs,zs) = ([x | x<-ys, x<=y], [z | z<-ys, z> y]) `using` parTuple2 rpar rpar
+    -- where (xs,zs) = partition (<= y) ys `using` parTuple2 rpar rpar
+
+n = 50000000
+
+main :: IO ()
+main = do
+    let x = last . qsort $ [n,n-2..2]++[1,3..n-1]
+    case x == x of True -> return ()
