1bdfe8eeeb00049af7408df279fc8cbdb316b30c
Ki Yung Ahn <kyagrd@gmail.com>
2021-05-26 19:37:08 +0900

    save

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,7 @@ module Main where
 import Lib
 
 import Control.Parallel.Strategies
--- import Data.List
 import Control.Monad.Trans.State.Strict
-import Control.Monad
-import Data.Functor.Identity
 
 qsort [] = []
 qsort [y] = [y]
@@ -30,30 +27,6 @@ data Expr
     | Lam Nm Expr
     | App Expr Expr
     deriving Show
-
-newId :: Monad m => StateT (Int, Int) m Int
-newId = do { (k,i) <- get; put (k+i,i); return k }
-
-runParallel :: [State (Int, Int) a] -> State (Int, Int) [a]
-runParallel ms = do
-    (k,i) <- get
-    let n = length ms
-    let ps = zipWith ($) [flip runState (k+r,i*n) | r<-[0..n-1]] ms
-              `using` parList rpar
-    let (as,ss) = unzip ps
-    put (maximum (map fst ss), i)
-    return as
-
-runParallelT :: Monad m => (m (a,(Int,Int)) -> (a,(Int,Int))) ->
-    [StateT (Int, Int) m a] -> StateT (Int, Int) m [a]
-runParallelT runM ms = do
-    (k,i) <- get
-    let n = length ms
-    let ps = zipWith ($) [runM . flip runStateT (k+r,i*n) | r<-[0..n-1]] ms
-              `using` parList rpar
-    let (as,ss) = unzip ps
-    put (maximum (map fst ss), i)
-    return as
 
 main = print $ runParallel
     [ sequence [newId]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,33 @@
 module Lib
-    ( someFunc
+    ( newId
+    , runParallel
+    , runParallelT
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Parallel.Strategies
+import Control.Monad.Trans.State.Strict
+
+newId :: Monad m => StateT (Int, Int) m Int
+newId = do { (k,i) <- get; put (k+i,i); return k }
+
+runParallel :: [State (Int, Int) a] -> State (Int, Int) [a]
+runParallel ms = do
+    (k,i) <- get
+    let n = length ms
+    let ps = zipWith ($) [flip runState (k+r,i*n) | r<-[0..n-1]] ms
+              `using` parList rpar
+    let (as,ss) = unzip ps
+    put (maximum (map fst ss), i)
+    return as
+
+runParallelT :: Monad m => (m (a,(Int,Int)) -> (a,(Int,Int))) ->
+    [StateT (Int, Int) m a] -> StateT (Int, Int) m [a]
+runParallelT runM ms = do
+    (k,i) <- get
+    let n = length ms
+    let ps = zipWith ($) [runM . flip runStateT (k+r,i*n) | r<-[0..n-1]] ms
+              `using` parList rpar
+    let (as,ss) = unzip ps
+    put (maximum (map fst ss), i)
+    return as
+
